7b9d5bc8a7e2c4895f06f1174b8e4b28d6903da9
Purefekt <roonyyo007@gmail.com>
2022-09-21 18:25:01 -0700

    seeing data on javascript console

--- hw6/homework_6/main.py
+++ hw6/homework_6/main.py
@@ -1,12 +1,19 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return app.send_static_file('main.html')
 
+
+@app.route('/search_yelp')
+def search_yelp():
+    data = requests.args
+    for k,v in data.items():
+        print(f"{k} : {v}")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
---
+++ hw6/homework_6/static/main.js
@@ -0,0 +1,11 @@
+function search_yelp(initial_form) {
+    var form_keyword = initial_form.input_keyword.value;
+    var form_distance = initial_form.input_distance.value;
+    var form_category = initial_form.select_category.value;
+    var form_location = initial_form.input_location.value;
+
+    console.log(form_keyword);
+    console.log(form_distance);
+    console.log(form_category);
+    console.log(form_location);
+}
