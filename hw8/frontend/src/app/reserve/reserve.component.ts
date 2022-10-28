f77369494aab7cdf22c42ca1ee1aad938c21d241
Purefekt <roonyyo007@gmail.com>
2022-10-28 02:02:50 -0700

    form validation is hard bro

--- hw8/frontend/src/app/reserve/reserve.component.ts
+++ hw8/frontend/src/app/reserve/reserve.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {
   NgbDatepickerConfig,
   NgbDateAdapter,
   NgbDateStruct,
   NgbDateParserFormatter,
 } from '@ng-bootstrap/ng-bootstrap';
+
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+
+// to change date format
 @Injectable()
 export class CustomDateAdapter {
   fromModel(value: string): NgbDateStruct {
@@ -64,7 +73,8 @@ export class CustomDateParserFormatter {
 export class ReserveComponent implements OnInit {
   constructor(
     private modalService: NgbModal,
-    private config: NgbDatepickerConfig
+    private config: NgbDatepickerConfig,
+    private formBuilder: FormBuilder
   ) {
     const current = new Date();
     config.minDate = {
@@ -75,35 +85,60 @@ export class ReserveComponent implements OnInit {
     config.outsideDays = 'hidden';
   }
 
+  form!: FormGroup;
+
   calendar_date_input: any;
+
   print_calendar_date_input() {
     console.log(this.calendar_date_input);
   }
 
-  close_result = '';
+  print_errors() {
+    console.log('email errors');
+    console.log(this.form.get('email')?.errors);
+    console.log('date errors');
+    console.log(this.form.get('date')?.errors);
+  }
+
   open_modal(content: any) {
     this.modalService
       .open(content, { ariaLabelledBy: 'modal-basic-title' })
       .result.then(
-        (result) => {
-          this.calendar_date_input = null;
-          this.close_result = `Closed with ${result}`;
-        },
-        (reason) => {
-          this.calendar_date_input = null;
-          this.close_result = `Dismissed ${this.get_dismissed_reason(reason)}`;
-        }
+        () => {},
+        () => {}
       );
   }
-  private get_dismissed_reason(reason: any) {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
+
+  isFieldValid(field: string) {
+    return !this.form.get(field)!.valid && this.form.get(field)!.touched;
+  }
+
+  // validate form on submit
+
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+  onSubmit() {
+    console.log(this.form);
+    if (this.form.valid) {
+      console.log('form submitted');
     } else {
-      return `with: ${reason}`;
+      this.validateAllFormFields(this.form);
+      console.log('form not submitted');
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      date: [, [Validators.required]],
+    });
+  }
 }
